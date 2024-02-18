9f2360cfbb694ee1e27bbd14000aa56cb7f1de43
Alvaro Montesinos <aamontesinosm@gmail.com>
2024-02-17 22:12:28 -0400

    feature: imporves project

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request,render_template, json
-import pickle
-import pandas as pd
+from flask import Flask, request, render_template
 import os
 import cv2
 import numpy as np
@@ -10,30 +8,37 @@ app = Flask(__name__)
 
 model = tf.keras.models.load_model('ml/my_model')
 
-@app.route('/',methods=["Get","POST"])
+target_labels = ["Gato", "Perro"]
+
+@app.route('/', methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
-@app.route('/predict',methods=["Get","POST"])
+@app.route('/predict', methods=["POST"])
 def predict():
     new_file = request.files['file']
-    target_path = os.path.join("upload",new_file.filename)
+    target_path = os.path.join("upload", new_file.filename)
     new_file.save(target_path)
 
-    image = cv2.imread(target_path, 0)
-    image = data_validation(image)
+    image = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
 
-    prediction = model.predict(image)
-    predicted_label = np.argmax(prediction, 1)
-    return f"Es un {predicted_label[0]} ;) !!"
+    resized_image = cv2.resize(image, (100, 100))
+
+    image_data = data_validation(resized_image)
+
+    prediction = model.predict(image_data)
+    predicted_label = np.argmax(prediction, axis=1)[0]
+
+    return f"Edsf un {target_labels[predicted_label]} ;) !!"
 
 def data_validation(image):
-    # image = image.flatten()
     image = np.expand_dims(image, axis=0)
-    # Convertir la imagen a float32 para usar valores decimales en el tensor
+
     image = tf.cast(image, tf.float32)
-    # Dividir el tensor entre el nivel de intensidad mas alto en la imagen
+
+    image /= 255.0
+
     return image
 
 if __name__ == '__main__':
-     app.run(debug=True, port=5002)
+    app.run(debug=True, port=5002)
